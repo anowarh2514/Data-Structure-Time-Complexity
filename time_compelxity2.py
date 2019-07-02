9abb774f8ff10c997d6dbe11d9c2294efda88652
Md Anowar Hossain <anowarh761@gmail.com>
2019-07-03 00:59:05 +0600

    This is time complexity code in data structure

---
+++ time_compelxity2.py
@@ -0,0 +1,13 @@
+# this programs time complexity is n*n or n^2
+
+i=0;j=0;n=0
+
+n = input("Enter n number value: ")
+count = 0
+for i in range(int(n)):
+    for j in range(int(n)):
+        count = int(count) + 1
+for i in range(int(n)):
+    count = int(count) + 1
+# print(n)
+print(count)
